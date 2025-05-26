00a9127b44ac5e4fd2a8777a1d96ee97d076ead9
Pedro Reis <dropecosta@gmail.com>
2025-05-26 21:29:33 +0100

    Rewriting remove from cart endpoint

--- app/db.ts
+++ app/db.ts
@@ -12,6 +12,8 @@ export async function connectToDb() {
   const MONGODB_PASSWORD = '';
   const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0.af7ycek.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+  //const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.vqdwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
   const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
